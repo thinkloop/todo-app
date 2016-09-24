ccc9fac4cbfe0d6fd80dd51799dcdfe7bb35c4a6
Baz <bk@thinkloop.com>
2016-09-24 02:39:07 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import { state, actions, subscribe } from 'todo-redux-state';
-import { components } from 'todo-react-components';
-import * as PATHS from './site/constants/paths';
+import { component } from 'todo-react-components';
 
+import * as PATHS from './site/constants/paths';
 import selectors from './selectors';
 
-var appElement = document.getElementById('app');
-
+// debug stuff
 window.state = state;
 window.selectors = selectors;
 window.actions = actions;
 console.log('********************************************* \n DEVELOPMENT MODE \n window.state available \n window.selectors available \n ********************************************* \n');
 
-subscribe(() => components(appElement, selectors));
+// subscribe to state changes and re-render app on every change
+const htmlElement = document.getElementById('app');
+subscribe(() => component(htmlElement, selectors));
 
+// read the url and navigate to the right page
 const initialSelectedPage = Object.keys(PATHS).find(key => PATHS[key] === window.location.pathname) || PATHS.HOME;
 actions.site.updateSelectedPage(initialSelectedPage);
+
+// load todos
 actions.todos.loadTodos();
 
-
+// listen for back button, forward button, etc
+window.onpopstate = (e) => {
+    const newSelectedPage = Object.keys(PATHS).find(key => PATHS[key] === window.location.pathname) || PATHS.HOME;
+    actions.site.updateSelectedPage(newSelectedPage);
+};
